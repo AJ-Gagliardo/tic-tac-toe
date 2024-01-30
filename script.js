ca484850b152b90dd969ad2ea70d47f3d1b4eb34
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-01-30 12:03:29 -0800

    added a fuction to correctly check rows, also corrected the fuction to reset the game board

--- script.js
+++ script.js
@@ -48,11 +48,15 @@ function startBoard(){
 }
 
 function resetBoard(){
-    for(let i=0; i>Gameboard.rows; i++){
-        for(let j=0;j>Gameboard.columns; j++){
-            Gameboard.board[i][j] = 0;
-        }
-    }
+    // for(let i=0; i<Gameboard.rows; i++){
+    //     Gameboard
+    //     for(let j=0;j>Gameboard.columns; j++){
+    //         Gameboard.board[i][j] = 0;
+    //     }
+    // }
+    Gameboard.board=[];
+    startBoard();
+    
 }
 
 //  function to mark one of the cells
@@ -61,14 +65,46 @@ let activePlayer = 1;
 
 function select(row, column){
 
-Gameboard.board[row][column] = activePlayer;
+if(Gameboard.board[row][column] === 0){ //checks if the cell is empty or not
 
-activePlayer===1 ? activePlayer = 2 :  activePlayer =1
+    Gameboard.board[row][column] = activePlayer;
+    // activePlayer===1 ? activePlayer = 2 :  activePlayer =1; // changes the turn to the other player once the player do a move
+}
+
+else{
+    console.log('sorry cell already taken');
+    return; // doesn't let you put your token if cell is taken
+}
 
 }
 
-// Do something taht checks if that cell si empty or not
 
-// something to take turns
+
 
 // How the gae ends + Something to reset the cells once the game finishes
+
+// win conditios
+//1 row is filled with same token
+
+// function checkRows(){
+//     for(i=0; i>rows;i++){
+//         Gameboard.board[i][0]
+//     }
+// }
+
+
+// Gameboard.board[0][]
+
+
+function checkRows() {
+    for (let row = 0; row < Gameboard.rows; row++) {
+        if (
+            Gameboard.board[row][0] !== 0 &&
+            Gameboard.board[row][0] === Gameboard.board[row][1] &&
+            Gameboard.board[row][0] === Gameboard.board[row][2]
+        ) {
+            return Gameboard.board[row][0]; // Player 1 or 2 wins
+        }
+    }
+    return 0; // No winner in rows
+}
